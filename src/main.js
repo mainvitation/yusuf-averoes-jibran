bf95d872d5c9383f2c5a1c321be6cb6715ed9ccf
Fachri Riyanto <fachririyanto@gmail.com>
2023-06-18 12:01:06 +0700

    Update JS

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-window.onload = function () {
+let isSongLoaded = false;
+
+document.addEventListener('click', () => {
+    if (isSongLoaded) {
+        return;
+    }
+
     // Create an audio context
     const audioContext = new AudioContext();
 
@@ -12,8 +18,23 @@ window.onload = function () {
             source.buffer = decodedData;
             source.connect(audioContext.destination);
             source.start();
+
+            isSongLoaded = true;
         })
         .catch(error => {
             console.error('Error loading audio:', error);
         });
-};
+});
+
+/*window.onload = function () {
+    const audio = document.getElementById('hbd-song');
+    const buttonPlaySong = document.getElementById('button-play-song');
+
+    buttonPlaySong.addEventListener('click', function () {
+        audio.muted = false;
+        audio.play();
+    });
+
+    // trigger the click event
+    buttonPlaySong.click();
+};*/
