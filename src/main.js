17f670ae4d0cbf3d37bcfb4930d8c6a4cb0b0eaf
Fachri Riyanto <fachririyanto@gmail.com>
2023-06-19 06:39:56 +0700

    Latest dev

--- src/main.js
+++ src/main.js
@@ -1,7 +1,33 @@
-var sound = new Howl({
-    src: ['song/hbd.mp3'],
-    autoplay: true,
-    loop: true,
-});
+let isAudioPlaying = false;
+const audio = document.getElementById('music');
 
-sound.play();
+document.addEventListener('DOMContentLoaded', function() {
+
+    // handle click tab info
+    document.getElementById('tab-info').addEventListener('click', function(event) {
+        event.preventDefault();
+
+        const $tabInfo = document.getElementById('tab-info');
+        
+        document.getElementById('page-info').classList.toggle('hidden');
+        $tabInfo.classList.toggle('active');
+    });
+
+    // handle click tab audio
+    document.getElementById('tab-audio').addEventListener('click', function(event) {
+        event.preventDefault();
+
+        const $tabAudio = document.getElementById('tab-audio');
+        
+        // if audio is playing
+        if (isAudioPlaying) {
+            audio.pause();
+            isAudioPlaying = false;
+            $tabAudio.querySelector('.material-symbols-outlined').textContent = 'volume_off';
+        } else {
+            audio.play();
+            isAudioPlaying = true;
+            $tabAudio.querySelector('.material-symbols-outlined').textContent = 'volume_up';
+        }
+    });
+});
